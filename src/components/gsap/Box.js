3504fc3092c7de060eee9ab675ea48cd22622879
benny.hsieh <benny.hsieh@isuncloud.com>
2017-12-19 17:03:16 +0800

    update

--- src/components/gsap/Box.js
+++ src/components/gsap/Box.js
@@ -7,7 +7,8 @@ export default class Class extends Component {
   {
     // TweenMax.to(this.box, 2, { x: '+=200px', backgroundColor: '#2196f3' });
 
-    TweenLite.to(this.box, 1, {x: '+=200px'});
+    // TweenLite.to(this.box, 1, {x: '+=200px'});
+    TweenLite.to(this.box, 1, {opacity:0, delay:.5});
   }
 
   render() {
